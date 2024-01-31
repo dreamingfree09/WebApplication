5966f1bc238a8956e7a10dc679c0475414862ec4
dreamingfree09 <kostasdnds@outlook.com>
2024-01-31 01:16:06 +0000

    final

--- quiz/scoreboard.php
+++ quiz/scoreboard.php
@@ -1,68 +1,82 @@
 <?php include('../class/action.php'); ?>
 <!DOCTYPE html>
 <html>
+
 <head>
     <title>Scoreboard</title>
-    <meta charset="UTF-8"/>
-    <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
-    <meta name="description" content="Football Quiz scoreboard"/>
-    <link rel="stylesheet" href="../css/home-style.css"/>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <meta name="description" content="Football Quiz scoreboard" />
+    <link rel="stylesheet" href="../css/home-style.css" />
 </head>
+
 <body>
-<header>
-    <h1>Football Quiz Scoreboard</h1>
-    <nav>
-        <ul>
-            <li><a href="index.php">Home</a></li>
-            <li><a href="quiz.php">Take the Quiz</a></li>
-            <li>
-                <a href="scoreboard.php">View the Scoreboard</a>
-            </li>
-            <?php if (isset($_SESSION['userid']) && $_SESSION['usertype'] == "user") { ?>
-                <li><a href="scoreboard.php?logoutUser=true">LogOut</a></li>
-            <?php } else { ?>
-                <li><a href="login.php">Log In</a></li>
-                <li><a href="register.php">Register</a></li>
-            <?php } ?>
-        </ul>
-    </nav>
-</header>
-<main>
-    <table class="scoreboard-table">
-        <thead>
-        <tr>
-            <th>User</th>
-            <th>Correct Answer</th>
-            <th>Wrong Answer</th>
-            <th>Incomplete Answer</th>
-            <th>Date</th>
-        </tr>
-        </thead>
-        <tbody>
-        <?php
-        $sqlQuery = "SELECT res.id, us.name, res.correct_answer,res.wrong_answer,res.incomplete_answer,res.result_date  FROM `user_quiz_result` AS res INNER JOIN users AS us ON res.user_id = us.id";
-        $resultSet = mysqli_query($dbConnect, $sqlQuery);
-        $datas = mysqli_fetch_all($resultSet, MYSQLI_ASSOC);
-        ?>
-        <?php
-        if (!empty($datas) && count($datas) > 0) {
-            foreach ($datas as $key => $data) { ?>
+    <header>
+        <h1>Football Quiz Scoreboard</h1>
+        <nav>
+            <ul>
+                <li><a href="index.php">Home</a></li>
+                <li><a href="quiz.php">Take the Quiz</a></li>
+                <li>
+                    <a href="scoreboard.php">View the Scoreboard</a>
+                </li>
+                <?php if (isset($_SESSION['userid']) && $_SESSION['usertype'] == "user") { ?>
+                    <li><a href="scoreboard.php?logoutUser=true">LogOut</a></li>
+                <?php } else { ?>
+                    <li><a href="login.php">Log In</a></li>
+                    <li><a href="register.php">Register</a></li>
+                <?php } ?>
+            </ul>
+        </nav>
+    </header>
+    <main>
+        <table class="scoreboard-table">
+            <thead>
                 <tr>
-                    <td><?php echo $data['name']; ?></td>
-                    <td><?php echo $data['correct_answer']; ?></td>
-                    <td><?php echo $data['wrong_answer']; ?></td>
-                    <td><?php echo $data['incomplete_answer']; ?></td>
-                    <td><?php echo $data['result_date']; ?></td>
+                    <th>User</th>
+                    <th>Correct Answer</th>
+                    <th>Wrong Answer</th>
+                    <th>Incomplete Answer</th>
+                    <th>Date</th>
                 </tr>
-            <?php }
-        } ?>
-        <!-- Add more rows for other users -->
-        </tbody>
-    </table>
-</main>
-<footer>
-    <p>© <span id="currentYear"></span> Football Quiz</p>
-</footer>
-<script src="../js/Home-script.js"></script>
+            </thead>
+            <tbody>
+                <?php
+                $sqlQuery = "SELECT us.name, res.correct_answer, res.wrong_answer, res.incomplete_answer, res.result_date FROM user_quiz_result AS res INNER JOIN users AS us ON res.user_id = us.id ORDER BY res.correct_answer DESC LIMIT 10";
+
+                $resultSet = mysqli_query($dbConnect, $sqlQuery);
+                $datas = mysqli_fetch_all($resultSet, MYSQLI_ASSOC);
+                ?>
+                <?php
+                if (!empty($datas) && count($datas) > 0) {
+                    foreach ($datas as $key => $data) { ?>
+                        <tr>
+                            <td>
+                                <?php echo $data['name']; ?>
+                            </td>
+                            <td>
+                                <?php echo $data['correct_answer']; ?>
+                            </td>
+                            <td>
+                                <?php echo $data['wrong_answer']; ?>
+                            </td>
+                            <td>
+                                <?php echo $data['incomplete_answer']; ?>
+                            </td>
+                            <td>
+                                <?php echo $data['result_date']; ?>
+                            </td>
+                        </tr>
+                    <?php }
+                } ?>
+                <!-- Add more rows for other users -->
+            </tbody>
+        </table>
+    </main>
+    <footer>
+        <p>© <span id="currentYear"></span> Football Quiz</p>
+    </footer>
+    <script src="../js/Home-script.js"></script>
 </body>
+
 </html>
